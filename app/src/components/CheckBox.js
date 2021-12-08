c2695ef1d3ab441bb4ee5e49ed8cc8caa5c2ba69
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2021-12-08 19:01:21 +0100

    Added animation to the CheckBox component

--- app/src/components/CheckBox.js
+++ app/src/components/CheckBox.js
@@ -1,22 +1,43 @@
 import React, {useState} from "react";
+import { animated, useSpring} from "react-spring";
 
 import "../css/CheckBox.css";
 
 const CheckBox = () => {
 
+    // Defining the variables
     const [isChecked, setIsChecked] = useState(false);
+    const [checkmarkLength, setCheckmarkLength] = useState(null);
 
     // Creating a function that will allow to toggle the checkbox
     const toggleCheckBox = () => { 
         setIsChecked(!isChecked);
     }
 
+    // Creating a function for animating the checkbox
+    const checkboxAnimationStyle = useSpring({
+        backgroundColor: isChecked ? "#4bb543" : "#fff",
+        borderColor: isChecked ? "#4bb543" : "ddd"
+    }); 
+
+    // Creating a function for animating the checkmark
+    const checkmarkAnimationStyle = useSpring({
+        x: isChecked ? 0 : checkmarkLength
+    });
+
+    // Creating a function that will mesure the length of the checkmark
+    const setRef = (ref) => {
+        if(ref){
+            setCheckmarkLength(ref.getTotalLength())
+        }
+    }
+
     return (
         <label>
             <input type="checkbox" onChange={toggleCheckBox} className="checkbox-value"/>
-            <svg className={`checkbox ${isChecked ? "checked": ""}`} aria-hidden="true" viewBox="-1 0 17 10" fill="none">
-                <path d="M1 4.5L5 9L14 1" strokeWidth="2" stroke={isChecked ? "#fff" : "none"}/>
-            </svg>
+            <animated.svg style={checkboxAnimationStyle} className={`checkbox ${isChecked ? "checked": ""}`} aria-hidden="true" viewBox="-1 0 17 10" fill="none">
+                <animated.path ref={setRef} d="M1 4.5L5 9L14 1" strokeWidth="2" stroke="#fff" strokeDasharray={checkmarkLength} strokeDashoffset={checkmarkAnimationStyle.x}/>
+            </animated.svg>
         </label>
     );
 };
