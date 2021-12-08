0ca9e45719e2c4b533be05c2539f4a3cb1dee369
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2021-12-08 18:43:18 +0100

    Modified the CheckBox component and styling

--- app/src/components/CheckBox.js
+++ app/src/components/CheckBox.js
@@ -11,15 +11,13 @@ const CheckBox = () => {
         setIsChecked(!isChecked);
     }
 
-    // Creating funciton that will handle the keypress
-    const handleKeyPress = (event) => {
-        if(event.key === "Enter" || event.key === " ") toggleCheckBox()
-    }
-
     return (
-        <div id="checkbox" onClick={toggleCheckBox} className={isChecked ? "checked" : ""} tabIndex="1" onKeyUp={handleKeyPress}>
-            {isChecked && <span id="checkmark">✓</span>}
-        </div>
+        <label>
+            <input type="checkbox" onChange={toggleCheckBox} className="checkbox-value"/>
+            <svg className={`checkbox ${isChecked ? "checked": ""}`} aria-hidden="true" viewBox="-1 0 17 10" fill="none">
+                <path d="M1 4.5L5 9L14 1" strokeWidth="2" stroke={isChecked ? "#fff" : "none"}/>
+            </svg>
+        </label>
     );
 };
 
