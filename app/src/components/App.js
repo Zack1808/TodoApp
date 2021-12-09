2c232993b086df535f52a50d8e60a008fae97870
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2021-12-09 18:16:33 +0100

    Added Button component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import CheckBox from "./CheckBox";
+import Button from "./Button";
 
 const App = () => {
     return(
         <div>
             <CheckBox />
             <CheckBox />
+            <Button />
         </div>
     )
 };
