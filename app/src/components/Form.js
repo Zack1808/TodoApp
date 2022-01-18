3f9a229aa80fed60a35e7050e829557a38e34a44
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-01-18 19:55:28 +0100

    Modified the Form component

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Button from './Button';
 import "../css/Input.css"
 
-const Form = ({ placeholder = "Enter text", btnText = "Enter button text"}) => {
+const Form = ({ placeholder = "Enter text", btnText = "Enter button text", onSubmit }) => {
+
+    // Creating a variable that stores the input value from the form
+    const [inputData, setInputData] = useState("");
+
+    // Function that changes the value of the input field 
+    const changeInputValue = (event) => {
+        setInputData(event.target.value);
+    }
+
+    // Function that will prevent the page from reloading after submition, send the input data to the parent and clear the input field
+    const formSubmition = (event) => {
+        event.preventDefault();
+        onSubmit(inputData);
+        setInputData("")
+    }
+
     return(
-        <form style={{display: "flex"}}>
-            <input className="input" type="text" placeholder={placeholder}></input>
+        <form style={{display: "flex"}} onSubmit={formSubmition} >
+            <input value={inputData} onChange={changeInputValue} className="input" type="text" placeholder={placeholder}></input>
             <Button type="submit" buttonStyle="btn--success--solid" buttonSize="btn--medium">{btnText}</Button>
         </form>
     )
--- app/src/components/ProjectListPage.js
+++ app/src/components/ProjectListPage.js
@@ -3,9 +3,14 @@ import React from "react";
 import Form from './Form';
 
 const ProjectListPage = () => {
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return(
         <div>
-            <Form placeholder="Enter Project Name" btnText="Create Project" />
+            <Form placeholder="Enter Project Name" btnText="Create Project" onSubmit={onSubmit} />
         </div>
     )
 }
